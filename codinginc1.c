651fbaea27329fd7dbf30df299db55f4e69972ba
umair khan <46400274+umairkhan02@users.noreply.github.com>
2020-10-16 16:01:20 +0530

    Update codinginc1.c
    changes1

--- codinginc1.c
+++ codinginc1.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main()
  {
-    double a, b, product;
-    printf("Enter two numbers: ");
+    double a, b, prod;
+    printf("\n Enter two numbers: ");
     scanf("%lf %lf", &a, &b);  
  
     // Calculating product
-    product = a * b;
+    prod = a * b;
 
     // Result up to 2 decimal point is displayed using %.2lf
-    printf("Product = %.2lf", product);
-    
+    printf("\n Product = %.2lf", prod);
     return 0;
 }
